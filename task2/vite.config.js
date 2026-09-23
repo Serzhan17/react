20518137ec38ccfb432ecfd6e389e64a9e07eb96
Serzhan17 <s_abdykadyr@kbtu.kz>
2026-09-24 01:21:04 +0500

    Update vite.config.js

--- task2/vite.config.js
+++ task2/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // IMPORTANT for GitHub Pages:
-// Set `base` to "/<your-repo-name>/" (with leading and trailing slashes)
+// Set `base` to "/<react>/" (with leading and trailing slashes)
 // e.g. if your repo is github.com/serzhan/about-me -> base: '/about-me/'
 // If you deploy to a *user* page (username.github.io repo itself), use '/'.
 export default defineConfig({
